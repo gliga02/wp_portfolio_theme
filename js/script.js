ebe7df564f66dcd6d413a61e863366bbc3662cf7
Djordje Gligorijevic <itdjordjegligorijevic@gmail.com>
2020-09-28 14:44:45 +0200

    final commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+$(".open").click(function () {
+    $(".wrapper").fadeIn(400);
+    $("body").css("overflow", "hidden");
+});
+
+$(".close").click(function () {
+    $(".wrapper").fadeOut(400);
+    $("body").css("overflow", "scroll");
+}); 
+
+$(".open-nav").click(function () {
+    $(".wrapper-nav").fadeIn(400);
+    $("body").css("overflow", "hidden");
+    $(".open-nav").css("display", "none");
+});
+
+$(".close").click(function () {
+    $(".wrapper-nav").fadeOut(400);
+    $("body").css("overflow", "scroll");
+    $(".open-nav").css("display", "inline");
+}); 
